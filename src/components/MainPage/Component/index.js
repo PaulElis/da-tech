d49b992b1957b73ccf773d706a5b753ca29a8008
github username <elis.paul7@gmail.com>
2019-01-12 09:54:31 -0500

    created component files and structures

---
+++ src/components/MainPage/Component/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class MainPage extends Component {
+
+  render() {
+    // console.log('props', this.props);
+    return (
+      <div>MainPage</div>
+    );
+  }
+}
+
+export default MainPage;
